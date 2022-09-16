76cb3c304acf6342e0c799e86845f0f26dd73991
Munbear <pdwssf@gmail.com>
2022-09-16 20:17:16 +0900

    plan map 2

--- src/viewModel/mapViewModel.js
+++ src/viewModel/mapViewModel.js
@@ -1,17 +1,17 @@
-import {getMapItems} from "../recoil/state";
-import {useRecoilValue} from "recoil";
-
-class MapData {
-    constructor() {
-
-    }
-
-    getMapData() {
-        const data = useRecoilValue(getMapItems);
-    }
-
-
-    getMapDataName(name) {
-        return getMapItems.filter((vegen) => vegen.name === name)
-    }
-}
+// import {getMapItems} from "../recoil/state";
+// import {useRecoilValue} from "recoil";
+//
+// class MapData {
+//     constructor() {
+//
+//     }
+//
+//     getMapData() {
+//         const data = useRecoilValue(getMapItems);
+//     }
+//
+//
+//     getMapDataName(name) {
+//         return getMapItems.filter((vegen) => vegen.name === name)
+//     }
+// }
